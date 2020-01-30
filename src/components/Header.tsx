837c999a6eedd2f3a6bb990a6630db4c0ae5ddd6
lurtt <lurttik@gmail.com>
2020-01-30 21:54:51 +0100

    create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React, { FC, ChangeEvent } from 'react'
+import { useHistory } from "react-router-dom"
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import MessageIcon from '@material-ui/icons/Message'
+import FaceIcon from '@material-ui/icons/Face'
+
+const Header: FC = () => {
+  const history = useHistory()
+
+  const handleCallToRouter = (event: ChangeEvent<{}>, route: string) => {
+    history.push(route)
+  }
+
+  return (
+    <Paper square data-testid="app-header">
+      <Tabs
+        value={history.location.pathname}
+        onChange={handleCallToRouter}
+        variant="fullWidth"
+        indicatorColor="secondary"
+        textColor="secondary"
+        aria-label="icon label tabs example"
+      >
+        <Tab icon={<FaceIcon />} label="USERS" value="/"/>
+        <Tab icon={<MessageIcon />} label="POSTS" value="/posts"/>
+      </Tabs>
+    </Paper>
+  )
+}
+
+export default Header
---
+++ src/components/__tests__/Header.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+import Header from 'components/Header'
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    location: {
+      pathname: '/'
+    },
+  }),
+}));
+
+describe('<Header />', () => {
+  it('should render', () => {
+    const { getByTestId } = render(<Header />)
+
+    expect(getByTestId('app-header')).toBeInTheDocument()
+  })
+})
