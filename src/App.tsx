a7d55cfa84202f1867a89324b11c76479c8caa17
lurtt <lurttik@gmail.com>
2020-01-31 21:41:29 +0100

    added swr global config

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React, { FC } from 'react'
+import React, { FC, Suspense } from 'react'
 import Container from '@material-ui/core/Container'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import { SWRConfig } from 'swr'
 
+import { typicodeAPI } from 'utils/api'
 import Header from 'components/Header'
 import Users from 'pages/Users'
 import Posts from 'pages/Posts'
 
 const App: FC = () => (
-  <Router>
-    <Container maxWidth="md" data-testid="app">
-      <Header />
-
-      <Switch>
-        <Route path="/posts">
-          <Posts />
-        </Route>
-        <Route path="/">
-          <Users />
-        </Route>
-      </Switch>
-    </Container>
-  </Router>
+  <SWRConfig value={{ fetcher: typicodeAPI }}>
+    <Router>
+      <Container maxWidth="md" data-testid="app">
+        <Header />
+        <Suspense fallback={<h1>LOADING...</h1>}>
+          <Switch>
+            <Route path="/posts">
+              <Posts />
+            </Route>
+            <Route path="/">
+              <Users />
+            </Route>
+          </Switch>
+        </Suspense>
+      </Container>
+    </Router>
+  </SWRConfig>
 )
 
 export default App
