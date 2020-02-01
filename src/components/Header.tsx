944a6b83b951ff37b80301a0032134d54f00d195
lurtt <lurttik@gmail.com>
2020-02-01 14:05:39 +0100

    fix error during user detail routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ChangeEvent } from 'react'
+import React, { FC, ChangeEvent, useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import Paper from '@material-ui/core/Paper'
 import Tabs from '@material-ui/core/Tabs'
@@ -6,8 +6,19 @@ import Tab from '@material-ui/core/Tab'
 import MessageIcon from '@material-ui/icons/Message'
 import FaceIcon from '@material-ui/icons/Face'
 
+import { ROUTE } from 'utils/api'
+
+const { HOME, POSTS } = ROUTE
+
 const Header: FC = () => {
   const history = useHistory()
+  const [activeTab, setActiveTab] = useState(HOME)
+
+  useEffect(() => {
+    const isPostActive = history.location.pathname.includes(POSTS)
+
+    setActiveTab(isPostActive ? POSTS : HOME)
+  }, [history.location.pathname])
 
   const handleCallToRouter = (_: ChangeEvent<{}>, route: string) => {
     history.push(route)
@@ -16,15 +27,15 @@ const Header: FC = () => {
   return (
     <Paper square data-testid="app-header">
       <Tabs
-        value={history.location.pathname}
+        value={activeTab}
         onChange={handleCallToRouter}
         variant="fullWidth"
         indicatorColor="secondary"
         textColor="secondary"
         aria-label="icon label tabs example"
       >
-        <Tab icon={<FaceIcon />} label="USERS" value="/" />
-        <Tab icon={<MessageIcon />} label="POSTS" value="/posts" />
+        <Tab icon={<FaceIcon />} label="USERS" value={HOME} />
+        <Tab icon={<MessageIcon />} label="POSTS" value={POSTS} />
       </Tabs>
     </Paper>
   )
