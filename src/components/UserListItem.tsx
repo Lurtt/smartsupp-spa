8c1cf2233551a1986d0bfa72a21dbd73a9efaba9
lurtt <lurttik@gmail.com>
2020-02-02 20:36:07 +0100

    polishing design

--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -5,6 +5,7 @@ import ListItemText from '@material-ui/core/ListItemText'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import Tooltip from '@material-ui/core/Tooltip'
+import Card from '@material-ui/core/Card'
 import { useHistory } from 'react-router-dom'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
 
@@ -34,16 +35,18 @@ const UserListItem: FC<Props> = ({ id, name, username, email }) => {
   }
 
   return (
-    <ListItem button onClick={handleCallToRouter} data-testid="user">
-      <ListItemAvatar>
-        <Tooltip title={name} enterDelay={350} leaveDelay={200} aria-label="user name">
-          <Avatar className={classes.color} data-testid="user-avatar">
-            {username.charAt(0)}
-          </Avatar>
-        </Tooltip>
-      </ListItemAvatar>
-      <ListItemText primary={username} secondary={email} data-testid="text" />
-    </ListItem>
+    <Card style={{ marginBottom: '1rem' }}>
+      <ListItem button onClick={handleCallToRouter} data-testid="user">
+        <ListItemAvatar>
+          <Tooltip title={name} enterDelay={350} leaveDelay={200} aria-label="user name">
+            <Avatar className={classes.color} data-testid="user-avatar">
+              {username.charAt(0)}
+            </Avatar>
+          </Tooltip>
+        </ListItemAvatar>
+        <ListItemText primary={username} secondary={email} data-testid="text" />
+      </ListItem>
+    </Card>
   )
 }
 
